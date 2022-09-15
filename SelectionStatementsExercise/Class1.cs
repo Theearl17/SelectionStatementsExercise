c27f70dc62506d00e69965c43fa5656e21f521ae
earl starks <earlstarks@rocketmail.com>
2022-09-14 21:51:24 -0500

    SelectionStatementsExercise

---
+++ SelectionStatementsExercise/Class1.cs
@@ -0,0 +1,25 @@
+﻿var r = new Random();
+var favoriteNumber = r.Next(1, 10);
+//var favoriteNumber = 17;
+
+Console.WriteLine("Pick a number 1-1000.");
+int userInput
+    /*= int.Parse(Console.ReadLine())*/;
+
+do
+{
+    if (userInput < favoriteNumber)
+    {
+        Console.WriteLine($" {userInput} is too low!");
+    }
+
+    else if (userInput > favoriteNumber)
+    {
+        Console.WriteLine($" {userInput} is too high!");
+    }
+    else
+    {
+        Console.WriteLine(" Go big money!!!! ");
+    }
+
+} while (userInput != favoriteNumber);
---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿var r = new Random();
+var favoriteNumber = r.Next(1, 10);
+Console.WriteLine("Pick a number 1-1000.");
+var userInput = int.Parse(Console.ReadLine());
+
+
+
+    if (userInput < favoriteNumber)
+    {
+        Console.WriteLine($" {userInput} is too low!");
+    }
+    else if (userInput > favoriteNumber)
+    {
+        Console.WriteLine($" {userInput} is too high!");
+    }
+    else
+    {
+        Console.WriteLine(" Go big money!!!! ");
+    }
+
